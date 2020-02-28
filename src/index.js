e5daf21ae7ebb35692aee1a4653e3b600805d0d2
hamletshu <ahngo13@naver.com>
2020-02-28 16:14:54 +0900

    Main.js 삭제 index.js로 통합

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {HashRouter} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Main from "./Main";
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
 
 ReactDOM.render(
-  <Main/>,
+  <HashRouter>
+    <Header/>
+    <Body/>
+    <Footer/>
+  </HashRouter>,
   document.querySelector("#container")
 );
