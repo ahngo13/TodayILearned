21248abb95d9e80d14bbf9e99c3e6998968c0e61
hamletshu <ahngo13@naver.com>
2020-03-01 01:48:05 +0900

    불필요한 소스 삭제

--- src/BoardForm.js
+++ src/BoardForm.js
@@ -8,43 +8,6 @@ axios.defaults.withCredentials = true;
 const headers = { withCredentials: true };
 
 class BoardRow extends Component {
-  state = {
-    board: []
-  };
-
-  getBoard = _id => {
-    const send_param = {
-      headers,
-      _id
-    };
-    axios
-      .post("http://localhost:8080/board/detail", send_param)
-      //정상 수행
-      .then(returnData => {
-        if (returnData.data.board[0]) {
-          const board = (
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>returnData.data.board[0].title</th>
-                </tr>
-              </thead>
-              <tbody></tbody>
-            </Table>
-          );
-          this.setState({
-            board: board
-          });
-        } else {
-          alert("글 상세 조회 실패");
-        }
-      })
-      //에러
-      .catch(err => {
-        console.log(err);
-      });
-  };
-  //onClick={this.getBoard.bind(null,this.props._id)}
   render() {
     return (
       <tr>
@@ -85,7 +48,7 @@ class BoardForm extends Component {
       .post("http://localhost:8080/board/getBoardList", send_param)
       .then(returnData => {
         if (returnData.data.list) {
-          console.log(returnData.data.list.length);
+          // console.log(returnData.data.list.length);
           const boards = returnData.data.list;
           const boardList = boards.map(item => (
             <BoardRow
